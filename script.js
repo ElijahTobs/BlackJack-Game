6f3cac24c2dcdfa8e0ee051f5eadbe425355dead
Elijah <eligenius@gmail.com>
2022-03-10 19:11:38 +0100

    Display different messages according to sum

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let sum = 0
+let message = ""
 let pickedCards = []
 let currentCard = document.getElementById("cards")
 let sumEl = document.getElementById("sum")
+let intro = document.getElementById("intro")
 
 
 function getRandomNumber() {
-  
+
   let random = Math.floor(Math.random() * 13) + 1
   if (random > 10) {
     return 10
@@ -32,4 +34,13 @@ function renderGame() {
   }
 
   sumEl.textContent = "Sum: " + sum
+
+  if (sum < 21) {
+    message = "Do you want to draw a new card"
+  } else if (sum === 21) {
+    message = "You've got a blackjack"
+  } else {
+    message = "Game Over!"
+  }
+  intro.textContent = message
 }
