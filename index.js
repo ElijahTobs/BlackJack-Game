533f5a8a693ea291be3660db82bb6687e9663fd7
Comfort Ige <eligenius@gmail.com>
2023-05-16 14:47:21 +0100

    Add startGame function and move HTML from js file

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
-const container = document.querySelector(".container");
 
-container.innerHTML = `
-      <h1>Blackjack</h1>
-      <p id="message-el">Want to play a round?</p>
-      <p id="cards-el">Cards:</p>
-      <p id="sum-el">Sum:</p>
-      <button onclick="startGame()">START GAME</button>
-`
+let secondCard = 4;
+let firstCard = 10;
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 
-let secondCard = 4
-let firstCard = 10
 
-if (sum <= 20) {
-  message = "Do you want to draw a new card?"
-} else if (sum === 21) {
-  message = "You've got Blackjack!"
-  hasBlackJack = true
-} else {
-  message = "You're out of the game!"
-  isAlive = false
+function startGame() {
+  sumEl.textContent = "Sum: " + sum
+  cardsEl.textContent = "Cards: " + firstCard + " " + secondCard  
+
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?"
+  } else if (sum === 21) {
+      message = "You've got Blackjack!"
+      hasBlackJack = true
+  } else {
+      message = "You're out of the game!"
+      isAlive = false
+  }
+
+
+  messageEl.textContent = message
 }
 
 
