8e89da8ceb00b2febd9b0abf82ec0d3247c3cbb4
Comfort Ige <eligenius@gmail.com>
2023-05-16 14:15:08 +0100

    Style button and add conditionals

--- index.js
+++ index.js
@@ -5,4 +5,29 @@ container.innerHTML = `
       <p id="message-el">Want to play a round?</p>
       <p id="cards-el">Cards:</p>
       <p id="sum-el">Sum:</p>
+      <button onclick="startGame()">START GAME</button>
 `
+
+let secondCard = 4
+let firstCard = 10
+
+if (sum <= 20) {
+  message = "Do you want to draw a new card?"
+} else if (sum === 21) {
+  message = "You've got Blackjack!"
+  hasBlackJack = true
+} else {
+  message = "You're out of the game!"
+  isAlive = false
+}
+
+
+
+
+
+
+
+
+
+
+
