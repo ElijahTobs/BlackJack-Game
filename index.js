25b183e40f1f64b0a252d9f75dffb77fd5e39862
Comfort Ige <eligenius@gmail.com>
2023-05-16 14:03:21 +0100

    Add HTML via js file

---
+++ index.js
@@ -0,0 +1,8 @@
+const body = document.querySelector(".container");
+
+body.innerHTML = `
+      <h1>Blackjack</h1>
+      <p id="message-el">Want to play a round?</p>
+      <p id="cards-el">Cards:</p>
+      <p id="sum-el">Sum:</p>
+`
