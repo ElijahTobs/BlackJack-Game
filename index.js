a345fd0f6672b55f2b641de7bcb793b2d9db446c
Comfort Ige <eligenius@gmail.com>
2023-05-16 20:31:05 +0100

    Update startGame and getRandomCard function

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 
-let firstCard = getRandomNumber();
-let secondCard = getRandomNumber();
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+let cards = []
+let sum = 0;
 let hasBlackJack = false
 let isAlive = true
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
-
 function startGame() {
+  isAlive = true
+
+  let firstNumber = getRandomCard()
+  let secondNumber = getRandomCard()
+  cards = [firstNumber, secondNumber]
+  sum = firstNumber + secondNumber
   renderGame()
 }
 
-function getRandomNumber() {
-  return Math.floor(Math.random() * 10)
+function getRandomCard() {
+  let randomNumber = Math.floor( Math.random()*13 ) + 1
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
+
 }
 
 function renderGame() {
@@ -24,7 +35,6 @@ function renderGame() {
   for (let x in cards) {
     cardsEl.textContent += cards[x] + " "
   }
-
 
   if (sum <= 20) {
     message = "Do you want to draw a new card?"
@@ -39,9 +49,8 @@ function renderGame() {
   messageEl.textContent = message
 }
 
-
 function newCard() {
-  let card = getRandomNumber();
+  let card = getRandomCard();
   sum += card;
 
   cards.push(card);
