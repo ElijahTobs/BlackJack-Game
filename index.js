b92ab981718032dc87444c954e91c45eee1e64d0
Comfort Ige <eligenius@gmail.com>
2023-05-16 14:10:13 +0100

    Add basic styling for HTML elements

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const body = document.querySelector(".container");
+const container = document.querySelector(".container");
 
-body.innerHTML = `
+container.innerHTML = `
       <h1>Blackjack</h1>
       <p id="message-el">Want to play a round?</p>
       <p id="cards-el">Cards:</p>
